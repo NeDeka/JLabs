085fbe2cc7260511818f51ac3687a0f0c5443a48
Eugene Lihimovich <elihimovic@gmail.com>
2021-12-12 16:35:29 +0300

    added MainFrame

--- src/bsu/rfe/java/group7/lab1/Lihimovich/varB10/MainFrame.java
+++ src/bsu/rfe/java/group7/lab1/Lihimovich/varB10/MainFrame.java
@@ -1,5 +1,7 @@
 package bsu.rfe.java.group7.lab1.Lihimovich.varB10;
 
+package bsu.rfe.java.group7.lab1.Lihimovich.varB10;
+
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import javax.swing.JOptionPane;
